2f4b026ab64399ed7d1c9d3e05abecdb5581ba3c
Bhuban Shrestha <bhuban.shrestha@cotiviti.com>
2021-04-13 20:10:38 +0545

    POS Business rule library added

---
+++ POS.BusinessRule/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.BusinessRule
+{
+    public class Class1
+    {
+    }
+}
