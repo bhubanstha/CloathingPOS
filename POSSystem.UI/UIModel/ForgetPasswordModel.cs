f73cf0b3a399b951945c32f4df8f13feeba40c3f
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-05-21 09:53:39 +0545

    Validation text changed

--- POSSystem.UI/UIModel/ForgetPasswordModel.cs
+++ POSSystem.UI/UIModel/ForgetPasswordModel.cs
@@ -18,7 +18,7 @@ namespace POSSystem.UI.UIModel
         [MaxLength(6, ErrorMessage = "Password can not be longer than 6 charactes long.")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Password is required.")]
+        [Required(ErrorMessage = "Confirmation Password is required.")]
         [MaxLength(6, ErrorMessage = "Password can not be longer than 6 charactes long.")]
         [Compare("Password", ErrorMessage = "Confirmation password and  password didn't match.")]
         public string ConfirmPassword { get; set; }
