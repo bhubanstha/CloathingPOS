4076778c0c4593fcc077558612338d3f2b1c9938
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-05-28 22:47:17 +0545

    PDF Viewer window added in project

---
+++ POSSystem.UI/PDFViewer/PdfPasswordDialog.xaml.cs
@@ -0,0 +1,71 @@
+/*! MoonPdf - A WPF-based PDF Viewer application that uses the MoonPdfLib library
+Copyright (C) 2013  (see AUTHORS file)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!*/
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoonPdf
+{
+    public partial class PdfPasswordDialog : MetroWindow
+    {
+        public string Password { get; private set; }
+
+        public PdfPasswordDialog()
+        {
+            InitializeComponent();
+            this.Loaded += PdfPasswordDialog_Loaded;
+        }
+
+        void PdfPasswordDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.txtPassword.Focus();
+            //this.txtPassword.SelectAll();
+        }
+
+        private void BtnApplyPassword_Click(object sender, RoutedEventArgs e)
+        {
+            this.Password = this.txtPassword.Password;
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtPassword_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Copy ||
+                e.Command == ApplicationCommands.Cut ||
+                e.Command == ApplicationCommands.Paste)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
