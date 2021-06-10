6d37757e0225f7cbf54f438f36f417ebed4e478d
bhubanstha <bhuban.shrestha@cotiviti.com>
2021-06-10 19:12:57 +0545

    transparent color returned instead of white color when color hex is not found

--- POSSystem.UI/Converter/ColorConverter.cs
+++ POSSystem.UI/Converter/ColorConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POSSystem.UI.Service;
+using System;
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
@@ -12,8 +13,9 @@ namespace POSSystem.UI.Converter
             string colorHex = value as string;
             Color? tempColor = MahApps.Metro.Controls.ColorHelper.ColorFromString(colorHex);
 
-            Color color = tempColor.HasValue ? tempColor.Value : Color.FromRgb(255, 255, 255);
+            Color color = tempColor.HasValue ? tempColor.Value : Color.FromScRgb(0, 255, 255, 255);
             SolidColorBrush brush = new SolidColorBrush(color);
+            //System.Drawing.Color.Transparent;
             return brush;
         }
 
