545f27880d188845f809bacdcc63e215cf812fd6
bhubanstha <bhuban.shrestha@cotiviti.com>
2021-06-10 19:22:38 +0545

    migration file added

---
+++ POS.Data/Migrations/202106080900138_addshop.cs
@@ -0,0 +1,31 @@
+﻿namespace POS.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addshop : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Shop",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 200),
+                        Address = c.String(nullable: false, maxLength: 200),
+                        PANNumber = c.String(maxLength: 20),
+                        LogoPath = c.String(maxLength: 200),
+                        CalculateVATOnSales = c.Boolean(nullable: false),
+                        PrintInvoice = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Shop");
+        }
+    }
+}
