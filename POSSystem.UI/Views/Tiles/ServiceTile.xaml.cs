179932e35135b4937f5354d3d653e6235f356241
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-06-12 16:46:41 +0545

    New dependency property added and xml formatted

--- POSSystem.UI/Views/Tiles/ServiceTile.xaml.cs
+++ POSSystem.UI/Views/Tiles/ServiceTile.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -12,13 +16,18 @@ namespace POSSystem.UI.Views.Tiles
     /// </summary>
     public partial class ServiceTile : UserControl
     {
-        //public string ServiceTitle { get; set; }
-        //public string ImageSource { get; set; }
-        //public string Description { get; set; }
 
 
+        //If you get 'dllimport unknown'-, then add 'using System.Runtime.InteropServices;'
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
 
-
+        public bool LoadImageFromResource
+        {
+            get { return (bool)GetValue(LoadImageFromResourceProperty); }
+            set { SetValue(LoadImageFromResourceProperty, value); }
+        }
 
         public string ServiceTitle
         {
@@ -26,21 +35,11 @@ namespace POSSystem.UI.Views.Tiles
             set { SetValue(ServiceTitleProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for ServiceTitle.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ServiceTitleProperty =
-            DependencyProperty.Register("ServiceTitle", typeof(string), typeof(ServiceTile), 
-                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ServiceTitlePropertyChange, null, false, UpdateSourceTrigger.PropertyChanged));
-
         public string Description
         {
             get { return (string)GetValue(DescriptionProperty); }
             set { SetValue(DescriptionProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for Description.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty DescriptionProperty =
-            DependencyProperty.Register("Description", typeof(string), typeof(ServiceTile),
-                 new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, DescriptionPropertyChanged, null, false, UpdateSourceTrigger.PropertyChanged));
 
 
 
@@ -49,6 +48,43 @@ namespace POSSystem.UI.Views.Tiles
             get { return (string)GetValue(ImageSourceProperty); }
             set { SetValue(ImageSourceProperty, value); }
         }
+
+
+
+        public Stretch ImageStretch
+        {
+            get { return (Stretch)GetValue(ImageStretchProperty); }
+            set { SetValue(ImageStretchProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageStretch.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageStretchProperty =
+            DependencyProperty.Register("Stretch", typeof(Stretch), typeof(ServiceTile), new FrameworkPropertyMetadata(Stretch.UniformToFill, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ImageStretchPropertyChange, null, false, UpdateSourceTrigger.PropertyChanged));
+
+
+
+
+
+        // Using a DependencyProperty as the backing store for LoadImageFromResource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LoadImageFromResourceProperty =
+            DependencyProperty.Register("LoadImageFromResource", typeof(bool), typeof(ServiceTile), new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, null, null, false, UpdateSourceTrigger.PropertyChanged));
+
+
+       
+        // Using a DependencyProperty as the backing store for ServiceTitle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ServiceTitleProperty =
+            DependencyProperty.Register("ServiceTitle", typeof(string), typeof(ServiceTile), 
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ServiceTitlePropertyChange, null, false, UpdateSourceTrigger.PropertyChanged));
+
+        
+
+        // Using a DependencyProperty as the backing store for Description.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(ServiceTile),
+                 new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, DescriptionPropertyChanged, null, false, UpdateSourceTrigger.PropertyChanged));
+
+
+
 
         // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ImageSourceProperty =
@@ -69,6 +105,16 @@ namespace POSSystem.UI.Views.Tiles
             lblServiceDescription.Text = Description;
         }
 
+
+        private static void ImageStretchPropertyChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ServiceTile st)
+            {
+                st.UpdateImageStretch();
+            }
+        }
+
+      
 
         private static void ServiceTitlePropertyChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -114,19 +160,49 @@ namespace POSSystem.UI.Views.Tiles
             imgServiceImage.Source = GetImageFromPath(ImageSource);
         }
 
+        private void UpdateImageStretch()
+        {
+            imgServiceImage.Stretch = ImageStretch;
+        }
+
         private ImageSource GetImageFromPath(string imageName)
         {
             if (!string.IsNullOrEmpty(imageName))
             {
-                BitmapImage image = new BitmapImage();
+                if (LoadImageFromResource)
+                {
+                    BitmapImage image = new BitmapImage();
 
-                image.BeginInit();
-                image.UriSource = new Uri("pack://application:,,,/POSSystem;component/" + imageName);
-                image.EndInit();
+                    image.BeginInit();
+                    image.UriSource = new Uri("pack://application:,,,/POSSystem;component/" + imageName);
+                    image.EndInit();
 
-                return image;
+                    return image;
+                }
+                else
+                {
+                    if (File.Exists(imageName))
+                    {
+                        using (System.Drawing.Bitmap img = new System.Drawing.Bitmap(imageName))
+                        {
+                            return ImageSourceFromBitmap(img);
+                        }
+                    }
+                    
+                }
             }
             return null;
+        }
+
+
+        public ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
         }
     }
 }
