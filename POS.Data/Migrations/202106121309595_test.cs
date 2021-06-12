c22741d9367b1a9ef363cefade7160babbfb71c2
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-06-12 19:01:55 +0545

    migration files added

---
+++ POS.Data/Migrations/202106121309595_test.cs
@@ -0,0 +1,18 @@
+﻿namespace POS.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Inventory", "Color", c => c.String(maxLength: 10));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Inventory", "Color", c => c.String(maxLength: 8));
+        }
+    }
+}
