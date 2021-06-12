858012a30362ad6e57f152780c19a271d2530679
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-06-12 19:01:42 +0545

    utilities extension added

---
+++ POS.Utilities/FilePath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace POS.Utilities
+{
+    public static class FilePath
+    {
+
+        public static string GetLogoFullPath(string logoName)
+        {
+            string logoPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Image", "CompanyLogo", logoName);
+            return logoPath;
+        }
+    }
+}
