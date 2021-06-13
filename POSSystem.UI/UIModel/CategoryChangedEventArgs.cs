a019642dd46f704a004a79eae6b764c7a6fd8d0a
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-06-13 14:09:10 +0545

    Category Changed Event and EventArgs added to communicate between Category Add module and Inventory Module

---
+++ POSSystem.UI/UIModel/CategoryChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using POS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSSystem.UI.UIModel
+{
+    public enum EventAction
+    {
+        Add,
+        Remove,
+        Update
+    }
+    public class CategoryChangedEventArgs
+    {
+        public Category Category { get; set; }
+        public EventAction Action { get; set; }
+    }
+}
