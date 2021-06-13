322f275385f0806f259d7dc1b735b9d4e2d60909
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-06-13 17:40:18 +0545

    migration added

---
+++ POS.Data/Migrations/202106131111330_inventory_history_changes.cs
@@ -0,0 +1,22 @@
+﻿namespace POS.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class inventory_history_changes : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.InventoryHistory", "PurchaseRate", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AddColumn("dbo.InventoryHistory", "RetailRate", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            DropColumn("dbo.InventoryHistory", "Rate");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.InventoryHistory", "Rate", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            DropColumn("dbo.InventoryHistory", "RetailRate");
+            DropColumn("dbo.InventoryHistory", "PurchaseRate");
+        }
+    }
+}
