795b95a204c8e05641741902e2699181b0cd9613
bhubanstha <bhuban.shrestha@cotiviti.com>
2021-06-14 17:10:24 +0545

    Event and Event Args added for Brand

---
+++ POSSystem.UI/UIModel/BrandChangedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using POS.Model;
+
+namespace POSSystem.UI.UIModel
+{
+    public class BrandChangedEventArgs
+    {
+        public Brand Brand { get; set; }
+        public EventAction Action { get; set; }
+    }
+
+
+}
