73ceec9589df3286728db89e0a75d76909c073fd
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-06-15 11:44:37 +0545

    new view created that contains only few properties of shop entity

---
+++ POSSystem.UI/Views/PartialShopView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using POSSystem.UI.ViewModel;
+using System.Windows.Controls;
+
+namespace POSSystem.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for PartialShopView.xaml
+    /// </summary>
+    public partial class PartialShopView : UserControl
+    {
+        private SettingViewModel model;
+        public PartialShopView()
+        {
+            InitializeComponent();
+            model = new SettingViewModel();
+            this.DataContext = model;
+        }
+    }
+}
