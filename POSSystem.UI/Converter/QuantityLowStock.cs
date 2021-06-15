c3a5ada4a78da7d708476523a08e104661463aac
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-06-15 12:32:09 +0545

    Low Stock converter added for styling

---
+++ POSSystem.UI/Converter/QuantityLowStock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace POSSystem.UI.Converter
+{
+    public class QuantityLowStock : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var qty = 0;
+            Int32.TryParse(value.ToString(), out qty);
+            return qty < 5;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
