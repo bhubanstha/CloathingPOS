af664d0d0b02766da00fbcb3d67fea2d22a88739
bhubanstha <bhuban.shrestha@cotiviti.com>
2021-06-15 17:39:05 +0545

    file name to image source utility added into utility library

---
+++ POS.Utilities/ImageUtility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace POS.Utilities
+{
+    public class ImageUtility
+    {
+        //If you get 'dllimport unknown'-, then add 'using System.Runtime.InteropServices;'
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool DeleteObject([In] IntPtr hObject);
+
+
+        public static ImageSource GetImageSource(string imageFullPath)
+        {
+            if(File.Exists(imageFullPath))
+            {
+                using (Bitmap img = new Bitmap(imageFullPath))
+                {
+                    return ImageSourceFromBitmap(img);
+                }
+            }
+            return null;
+        }
+
+        private static ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+    }
+}
