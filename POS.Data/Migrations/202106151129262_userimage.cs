759235a64cfcb02a5a8c7d0572a352ce571d11c8
bhubanstha <bhuban.shrestha@cotiviti.com>
2021-06-15 17:40:03 +0545

    migration files

---
+++ POS.Data/Migrations/202106151129262_userimage.cs
@@ -0,0 +1,22 @@
+﻿namespace POS.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class userimage : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.User", "ProfileImage", c => c.String(maxLength: 30));
+            AlterColumn("dbo.User", "DisplayName", c => c.String(nullable: false, maxLength: 40));
+            AlterColumn("dbo.User", "Password", c => c.String(nullable: false, maxLength: 60));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.User", "Password", c => c.String());
+            AlterColumn("dbo.User", "DisplayName", c => c.String());
+            DropColumn("dbo.User", "ProfileImage");
+        }
+    }
+}
