496ef6d09c7843f838fe89b6abbb85ece3bedba0
Bhuban Shrestha <bhuban.shrestha@cotiviti.com>
2021-07-08 14:35:44 +0545

    unused references removed

--- POSSystem.UI/ViewModel/SettingViewModel.cs
+++ POSSystem.UI/ViewModel/SettingViewModel.cs
@@ -5,10 +5,6 @@ using POS.Model;
 using POSSystem.UI.Service;
 using Prism.Commands;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace POSSystem.UI.ViewModel
