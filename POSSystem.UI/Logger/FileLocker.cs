fb10ff22d455ca0ba0e9de2388c10617c198663c
Bhuban Shrestha <bhuban.shrestha@cotiviti.com>
2021-07-08 15:42:17 +0545

    prevented to create empty log file

---
+++ POSSystem.UI/Logger/FileLocker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSSystem.UI
+{
+    public class FileLocker : log4net.Appender.FileAppender.MinimalLock
+    {
+
+        public override void ReleaseLock()
+        {
+            base.ReleaseLock();
+            var logFile = new FileInfo(CurrentAppender.File);
+            if (logFile.Exists && logFile.Length <= 0)
+            {
+                logFile.Delete();
+            }
+        }
+    }
+}
