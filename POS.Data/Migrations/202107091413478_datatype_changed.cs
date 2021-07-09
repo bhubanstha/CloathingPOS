c15a1f8fde4cbf2a9d41e304dfbba67e8b0fac8c
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-07-09 20:00:50 +0545

    inventory history purchase date datatype changed

---
+++ POS.Data/Migrations/202107091413478_datatype_changed.cs
@@ -0,0 +1,18 @@
+﻿namespace POS.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class datatype_changed : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.InventoryHistory", "PurchaseDate", c => c.DateTime(nullable: false, storeType: "date"));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.InventoryHistory", "PurchaseDate", c => c.DateTime(nullable: false));
+        }
+    }
+}
--- POS.Model/InventoryHistory.cs
+++ POS.Model/InventoryHistory.cs
@@ -8,6 +8,8 @@ namespace POS.Model
         public int Quantity { get; set; }
         public decimal PurchaseRate { get; set; }
         public decimal RetailRate { get; set; }
+
+        [Column(TypeName = "DATE")]
         public DateTime PurchaseDate { get; set; }
 
         [ForeignKey("Inventory")]
